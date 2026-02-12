90a29cd1c4a3e53190e3f3a0338278e9c0305a3b
Cesar <46138791+cesarmoo@users.noreply.github.com>
2026-02-11 21:17:21 -0500

    Update print statement from 'Hello' to 'Goodbye'

--- assets/js/main.js
+++ assets/js/main.js
@@ -55,14 +55,14 @@ $("#banner-div").ready(function() {
 			}]
 		};
 		var point_opts = { // index: [text, align, offset]
-							5: ["Mobile\nDevelopment", "right", 4],
-							14: ['"I know everything! :D"', 280, 5],
+							5: ["Mobile\nDevelopment", "right", 5],
+							14: ['"I know everything!"', 280, 5],
 							40: ["Web Development", "right", 4],
-							56: ['"I don\'t know anything :("', "bottom", 5],
-							68: ["API Development", "right", 4],
-							72: ["Distributed Systems", 13, 4],
-							80: ["Cloud Infrastructure", 205, 4],
-							90: ["C#, C++", 15, 4],
+							56: ['"I don\'t know anything..."', "bottom", 5],
+							68: ["API Development", "right", 5],
+							72: ["Distributed Systems", "right", 5],
+							80: ["Cloud Infrastructure", "left", 5],
+							90: ["C#, C++", 15, 5],
 							105: ["Learning new things", 215, 4],
 						 }
 
@@ -795,4 +795,5 @@ if (window.location.pathname == '/squotes') {
 
 
 })(jQuery);
+
 
